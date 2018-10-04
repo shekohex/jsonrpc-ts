f8348383e2d94f6ae164080a48433dec5c2a11be
Shady Khalifa <shekohex@gmail.com>
2018-10-05 01:48:11 +0200

    fix some types

--- src/rpc-client.ts
+++ src/rpc-client.ts
@@ -1,5 +1,6 @@
 import Axios, { AxiosInstance, AxiosResponse } from 'axios';
 import { RpcClientOptions, RpcRequest, RpcResponse } from './interfaces';
+import { RpcError } from './rpc-error';
 
 export class RpcClient<TMethods = any> {
   private readonly client: AxiosInstance = Axios;
@@ -9,24 +10,32 @@ export class RpcClient<TMethods = any> {
    * @param method the method to be called
    * @param params the method params
    * @param id and optinal json rpc request id
+   * @throws {AxiosError | RpcError} http/rpc error
    */
-  public async makeRequest<TResponse = any, TError = any>(
-    method: keyof TMethods,
-    params: TMethods[keyof TMethods],
+  public async makeRequest<TResponse, TError, K extends keyof TMethods = any >(
+    method: K,
+    params?: TMethods[K],
     id: string | number | null = null,
   ): Promise<AxiosResponse<RpcResponse<TResponse, TError>>> {
-    const request: RpcRequest<keyof TMethods, TMethods[keyof TMethods]> = {
+    const request: RpcRequest<K, TMethods[K]> = {
       jsonrpc: '2.0',
       method,
       params,
       id,
     };
-    const response = await this.client.post<RpcResponse<TResponse, TError>>(
-      this.options.url,
-      request,
-      this.options,
-    );
-    // TODO: Handle RPC Errors
-    return response;
+    try {
+      const response = await this.client.post<RpcResponse<TResponse, TError>>(
+        this.options.url,
+        request,
+        this.options,
+      );
+
+      if (response.data.error) {
+        throw new RpcError(response.data.error);
+      }
+      return response;
+    } catch (error) {
+      throw error;
+    }
   }
 }
